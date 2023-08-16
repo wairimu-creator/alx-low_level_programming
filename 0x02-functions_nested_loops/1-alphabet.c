0609c2b44e82cfb3833cfbb1c51d55f8ae705018
wairimu-creator <neemondungu17@gmail.com>
2023-08-16 08:10:17 -0700

    putchar

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+*print_alphabet - function that print alphabet
+*it prints in lowercase
+*Return: no return
+*/
+void print_alphabet(void)
+{
+	char ch;
+
+	for (ch = 'a' ; ch <= 'z' ; ch++)
+	{
+	_putchar(ch);
+	}
+
+	_putchar('\n');
+
+}
