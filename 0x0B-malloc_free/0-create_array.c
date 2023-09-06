b3aefa9e21e4baeef734c4c4e5b9ed696fa66d3a
wairimu-creator <neemondungu17@gmail.com>
2023-09-06 02:46:32 -0700

    main header file

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,32 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * main -  creates an array of chars initializes it with a specific char
+ * @size: size of the array
+ * @c: character
+ * Return: a p;ointer to the array or null
+*/
+
+char *create_array(unsigned int size, char c);
+{
+	char *array = NULL;
+	unsigned int i;
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+
+	char *array = malloc(sizeof(char) * size);
+	if (array == NULL)
+	{
+		return (NULL)
+	}
+		for (i = 0; i < size; i++)
+		{		
+			array[i] = c;
+		}
+
+	return (array);
+}
