b6d87f0342faea645367afce39fa7eaa62ec5dc3
wairimu-creator <neemondungu17@gmail.com>
2023-09-20 05:29:56 -0700

    in my dreams we are together

--- 0x08-recursion/1-print_rev_recursion.c
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -2,10 +2,11 @@
 
 /**
  * _print_rev_recursion - prints a string.
- *@s: sourcr string
+ * @s: string pointer
  *
- * Return: the string printed.
-**/
+ * Return: the string that will be printed
+ */
+
 void _print_rev_recursion(char *s)
 {
 	if (*s == '\0')
