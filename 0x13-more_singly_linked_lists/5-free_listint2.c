0b01b3e2006a2fcd5fbdffc66e8ba343a3a8f6cd
wairimu-creator <neemondungu17@gmail.com>
2023-09-26 14:08:00 -0700

    free

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -9,11 +9,15 @@ void free_listint2(listint_t **head)
 {
 	listint_t *temp;
 
-	temp = *head;
-	while (head != NULL)
+	if (head == NULL)
+		return;
+
+	while (*head)
 	{
 		temp = (*head)->next;
 		free(*head);
 		*head = temp;
 	}
+
+	*head = NULL;
 }
