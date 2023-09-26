08159d66b4e3b0956c7c8a7008541bab27a00cf7
wairimu-creator <neemondungu17@gmail.com>
2023-09-26 12:47:21 -0700

    list length

---
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * listint_len - returns the number of elements in a linked string
+ * @h: pointer to the head of the list
+ * Return: the number of elements in the list
+ */
+size_t listint_len(const listint_t *h)
+{
+	size_t nodes = 0;
+
+	while (h != NULL)
+	{
+		nodes++;
+		h = h->next;
+	}
+
+	return (nodes);
+}
